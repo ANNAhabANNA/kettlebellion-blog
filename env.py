69623b0d0c9acca74ad8547b7321913044b42727
Anna <akozorkova@gmail.com>
2022-06-22 15:17:46 +0000

    Store secret environment variables for secure operation

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ["DATABASE_URL"] = "postgres://jylpzfpmyteust:37e46e88b58f30781a347998683714347257b64ea9750959a6176ae26ca391f9@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/df2lp5bnde9qfb"
+os.environ["SECRET_KEY"] = "19811704Azz!"
