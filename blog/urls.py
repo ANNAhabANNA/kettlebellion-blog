98f343c5764e52b05cffaf4f3cd9f174e1befdbf
Anna <akozorkova@gmail.com>
2022-08-24 06:16:10 +0000

    Add post_detail.html and django-allauth functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # as_view method to render class as a view
     path("", views.PostList.as_view(), name="home"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
