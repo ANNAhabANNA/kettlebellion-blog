3a2af6313d725e84cf0d447ee93a54d743e986f2
Anna <akozorkova@gmail.com>
2022-08-28 16:56:19 +0000

    Allow logged-in users to like/unlike posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # as_view method to render class as a view
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
-from .models import Post
 from django.http import HttpResponseRedirect
+from .models import Post
 from .forms import CommentForm
 
 #Generic class-based view
@@ -32,7 +32,57 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
+                "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm()
             },
         )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists:
+            liked = True
+        
+        #Variable for saving data from the crispy form
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment = CommentForm()
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+class PostLike(View):
+
+    """
+    Creates the post like
+    """
+    
+    def post(self, request, slug, *args, **kwargs):
+        #Gets the relevant post
+        post = get_object_or_404(Post, slug=slug)
+        #Checks if the is already liked
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        #Reloads post_detail.html to see the result
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
