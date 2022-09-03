31e354c8e4b3687e55c912c67c56bb452e80eab2
Anna <akozorkova@gmail.com>
2022-09-03 14:37:06 +0000

    Debug summernote in admin panel not rendering

--- kettlebellion/urls.py
+++ kettlebellion/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog_urls"),
     path ('accounts/', include('allauth.urls')),
+    path('summernote/', include('django_summernote.urls')),
  
 ]
